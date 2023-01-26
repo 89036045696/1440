383543c7ba059545d2a6972ac206cdfb32f5c904
tokarev <tokarev@tokarev.ru>
2023-01-26 16:49:24 +0300

    Доработка по задачи "изменение логики обмена" - в работе.

--- FreeRTOS-Plus/Demo/my/myTasks/ConnectionTasks.c
+++ FreeRTOS-Plus/Demo/my/myTasks/ConnectionTasks.c
@@ -29,14 +29,12 @@
 QueueHandle_t QueueSendHandle;
 
 /*******************************************************************************
-* ¬озможные значени€ пол€ "команда" принимаемого пакета с командой.
+* ¬озможные значени€ пол€ "команда" принимаемого и отправл€емого пакета.
 *******************************************************************************/
 
-#define CMD_READ  ((uint8_t)  0)
+#define CMD_READ   ((uint8_t)  0)
 #define CMD_WRITE  ((uint8_t)  1)
-#define ACK_REPLY  ((uint8_t)  2)
-
-const uint8_t ACK_REQ = 1;
+#define CMD_ACK    ((uint8_t)  2)
 
 /*-----------------------------------------------------------*/
 #ifdef WIN32
@@ -98,6 +96,14 @@ union TagTxBuffer
 #pragma pack(pop)
 #endif
 
+enum ExchangeStatesIds
+{
+    ID_EXCHANGE_STATE__NONE               = 0,
+    ID_EXCHANGE_STATE__ACK_REQUEST,                 // инициирована отправка сообщени€ ACK_REQUEST
+    ID_EXCHANGE_STATE__WAIT_ACK_REPLY,              // инициирована отправка ответного сообщени€ и происходит ожидание ответа ACK_REPLY
+    ID_EXCHANGE_STATE__RECEIVED_REPEAT              // прин€то сообщение с повторным статусом (т.е. команду из пакета повторно выполн€ть не нужно и ответный пакет тоже уже сформирован - отправить ACk и исход€щий пакет)
+};
+
 
 void task1UDPConnection( void *pvParameters )
 {
@@ -107,10 +113,16 @@ void task1UDPConnection( void *pvParameters )
     union TagTxBuffer txBuf;
     while (1)
     {
+        /* ѕеред новым приЄмом 
+        ѕоиск свободного места в приЄмном буфере :
+        если свободное место есть, то принимаем новый пакет
+        если свободного места нет, то пропускаем шаг
+        */
+
         /* Read new RX packet with command */
 
-        // RX blocking time = infinity
-        const TickType_t rxTimeOut = portMAX_DELAY;
+        // RX blocking time = 0
+        const TickType_t rxTimeOut = 0;
         FreeRTOS_setsockopt(((struct TagParamsOfUDPConnectionTask*)pvParameters)->ClientSocket,
             0,
             FREERTOS_SO_RCVTIMEO,
@@ -118,7 +130,7 @@ void task1UDPConnection( void *pvParameters )
             sizeof(rxTimeOut));
 
         int32_t rxNum = FreeRTOS_recvfrom(((struct TagParamsOfUDPConnectionTask*)pvParameters)->ClientSocket,
-            &rxBuf, sizeof(rxBuf),
+            &rxBuf, sizeof(rxBuf) /* = максимальный размер вход€щего пакета */,
             0 /* ulFlags with the FREERTOS_ZERO_COPY bit clear. */,
             ((struct TagParamsOfUDPConnectionTask*)pvParameters)->PtrDestinationAddress,
             & ((struct TagParamsOfUDPConnectionTask*)pvParameters)->SourceAddressLength /* Not used but should be set as shown. */
@@ -136,8 +148,9 @@ void task1UDPConnection( void *pvParameters )
 
         /* Send REQ_ACK for "cmd" packets */
         txBuf.FormatAckRequest.Id = rxBuf.FormatCmdRead.Id;
-        txBuf.FormatAckRequest.AckRequest = ACK_REQ;
+        txBuf.FormatAckRequest.AckRequest = CMD_ACK;
         // TODO: 
+         /*TODO: размер пакета передавать в очереди*/
         while (pdTRUE != xQueueSendToBack(QueueSendHandle, &txBuf, 1)) { ; }
 
         /* Parse cmd packet, execute command and build tx packet */
@@ -146,7 +159,7 @@ void task1UDPConnection( void *pvParameters )
         {
         case CMD_READ:
             regReadValOrWriteStatus = reg_read( FreeRTOS_ntohl(rxBuf.FormatCmdRead.RegAddr) );
-            // ID установили, когда отсылали ACK_REQ
+            // ID транзакции установили в буфере, когда отсылали ACK_REQ
             txBuf.FormatCmdAnswer.RDataOrWStatus = FreeRTOS_htonl(regReadValOrWriteStatus);
             break;
 
@@ -169,8 +182,10 @@ void task1UDPConnection( void *pvParameters )
             &rxReplyAckTimeOut,
             sizeof(rxReplyAckTimeOut));
 
+        // TODO: переделать логику без блокировки
         for (int8_t nRepeats = 10; nRepeats > 0; nRepeats--)
         {
+            /*TODO: размер пакета tx передавать в очереди*/
             // TODO: 
             while (pdTRUE != xQueueSendToBack(QueueSendHandle, &txBuf, 1)) { ; }
 
@@ -183,7 +198,7 @@ void task1UDPConnection( void *pvParameters )
 
             if (rxNum != sizeof(rxBuf.FormatAckReply)) { continue; }
             if (rxBuf.FormatAckReply.Id != txBuf.FormatCmdAnswer.Id) { continue; }
-            if (rxBuf.FormatAckReply.AckReply != ACK_REPLY) { continue; }
+            if (rxBuf.FormatAckReply.AckReply != CMD_ACK) { continue; }
 
             break;
         }
@@ -195,17 +210,26 @@ void task2UDPConnection(void* pvParameters)
 {
     while (QueueSendHandle == NULL) { ; }
 
+    // max sendto timeout
+    const TickType_t txTimeOutMax = portMAX_DELAY;
+    FreeRTOS_setsockopt(((struct TagParamsOfUDPConnectionTask*)pvParameters)->ClientSocket,
+        0,
+        FREERTOS_SO_SNDTIMEO,
+        &txTimeOutMax,
+        sizeof(txTimeOutMax));
+
     union TagTxBuffer txBuf;
     while (1)
     {
         while (pdTRUE != xQueueReceive( QueueSendHandle, &txBuf, portMAX_DELAY)) { ; }
 
-
-        size_t sendLength = txBuf.bytes[1] == ACK_REQ ? sizeof(txBuf.FormatAckRequest) :
-            sizeof(txBuf.FormatCmdAnswer);
+        /*TODO: размер пакета tx передавать в очереди*/
+        // –азмер пакета отправл€емых данных разный, и зависит от кода команды в нЄм
+        size_t sendLength = txBuf.bytes[1] == CMD_ACK ? sizeof(txBuf.FormatAckRequest) :
+            /* CMD_READ or CMD_WRITE */ sizeof(txBuf.FormatCmdAnswer);
         FreeRTOS_sendto(((struct TagParamsOfUDPConnectionTask*)pvParameters)->ClientSocket,
             &txBuf,
-            txBuf.bytes[1] == ACK_REQ ? sizeof(txBuf.FormatAckRequest) : sizeof(txBuf.FormatCmdAnswer),
+            sendLength /*TODO: размер пакета передавать в очереди*/,
             0 /* ulFlags with the FREERTOS_ZERO_COPY bit clear. */,
             ((struct TagParamsOfUDPConnectionTask*)pvParameters)->PtrDestinationAddress,
             sizeof(struct freertos_sockaddr)
